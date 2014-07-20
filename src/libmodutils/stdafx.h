dedecfe5b0c9c3c3e736f30d0010cfbd13c55216
David Korth <gerbilsoft@gerbilsoft.com>
2014-07-20 14:03:51 -0400

    libmodutils: Actually add CMakeLists.txt, stdafx.cpp, and stdafx.h.
    I keep forgetting to add files to the repository.

---
+++ src/libmodutils/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+// Windows Header Files:
+#include <windows.h>
+
+// TODO: reference additional headers your program requires here
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "ninja.h"
