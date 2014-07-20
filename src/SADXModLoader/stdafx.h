1f508a271ad22164064ea5292345f50ff4324f15
David Korth <gerbilsoft@gerbilsoft.com>
2014-07-20 00:38:49 -0400

    Remove targetver.h
    targetver.h only has #include <SDKDDKVer.h>, which isn't needed by anything.

--- src/SADXModLoader/stdafx.h
+++ src/SADXModLoader/stdafx.h
@@ -5,13 +5,10 @@
 
 #pragma once
 
-#include "targetver.h"
-
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
 // Windows Header Files:
 #include <windows.h>
-
-
 
 // TODO: reference additional headers your program requires here
 #include <algorithm>
