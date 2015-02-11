71544779cc5bd022deb35245a0e0c69846a04b69
MainMemory <sonicmike2@yahoo.com>
2015-02-11 12:32:27 -0600

    Fixing building with MSVC projects.

--- cmake/platform/msvc.cmake
+++ cmake/platform/msvc.cmake
@@ -12,16 +12,15 @@ SET(MODLOADER_CXX_FLAGS_COMMON "${MODLOADER_C_FLAGS_COMMON}")
 
 # NOTE: Mod loader uses the high bit of addresses for other purposes.
 # SADX itself isn't large address aware, either.
-# FIXME: cmake-2.8.5 is required for /manifest:no to work correctly.
 # NOTE: -tsaware is automatically set for Windows 2000 and later. (as of at least Visual Studio .NET 2003)
-SET(MODLOADER_EXE_LINKER_FLAGS_COMMON "-nologo /manifest:no -subsystem:windows,5.01 -dynamicbase -nxcompat -largeaddressaware:no")
+SET(MODLOADER_EXE_LINKER_FLAGS_COMMON "-nologo -subsystem:windows,5.01 -dynamicbase -nxcompat -largeaddressaware:no")
 SET(MODLOADER_SHARED_LINKER_FLAGS_COMMON "${MODLOADER_EXE_LINKER_FLAGS_COMMON}")
 
 # Disable C++ RTTI and asynchronous exceptions.
 SET(MODLOADER_CXX_FLAGS_COMMON "${MODLOADER_CXX_FLAGS_COMMON} -GR- -EHsc")
 
 # Disable the RC and MASM "logo".
-SET(CMAKE_RC_FLAGS "-nologo")
+#SET(CMAKE_RC_FLAGS "-nologo")
 SET(CMAKE_ASM_MASM_FLAGS "-nologo")
 
 # Debug/release flags.
