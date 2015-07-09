2992883075f3e971534cb131543344435db50f3c
MainMemory <sonicmike2@yahoo.com>
2015-07-09 00:00:32 -0500

    Adding pre-build event, tweaking git_version.sh, updating .gitignore.

--- git_version.sh
+++ git_version.sh
@@ -174,10 +174,7 @@ fi
 git_repo=no
 # "git-rev-parse --git-dir" since git-0.99.7
 git_repo_dir="$($GIT rev-parse --git-dir 2> /dev/null || true)"
-abs_repo_dir="$(cd "$git_repo_dir" && pwd)"
-# Only accept the found git repo iff it is in our top srcdir, as determined
-# by comparing absolute pathnames created by running pwd in the respective dir.
-if [ "x$git_repo_dir" != "x" ] && [ "x${abs_repo_dir}" = "x${abs_srcdir}/.git" ]; then
+if [ "x$git_repo_dir" != "x" ]; then
     git_repo=yes
     if [ "x$git_found" = "xyes" ]; then
         # git-1.4 and probably earlier understand "git-rev-parse HEAD"
