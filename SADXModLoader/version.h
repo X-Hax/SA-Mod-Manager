c833803a79a745e294f10239ec53e14cfc9f6d9b
SonicFreak94 <sonicfreak94@hotmail.com>
2015-11-21 00:39:27 -0700

    Fixed version number string.

--- SADXModLoader/version.h
+++ SADXModLoader/version.h
@@ -4,7 +4,12 @@
 #define VERSION_MINOR 4
 #define VERSION_BUILD 0
 #define VERSION_REVISION 0
-#define makeverstr(major,minor) #major "." #minor
+//#define VERSION_PRERELEASE 1
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+#define makeverstr(major,minor) STRINGIFY(major) "." STRINGIFY(minor)
+
 #define VERSION_STRING makeverstr(VERSION_MAJOR, VERSION_MINOR)
-/*#define VERSION_PRERELEASE 1*/
 #define VERSION_WIN32 VERSION_MAJOR,VERSION_MINOR,VERSION_BUILD,VERSION_REVISION
