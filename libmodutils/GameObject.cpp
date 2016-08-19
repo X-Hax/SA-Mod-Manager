ca12bf0a86d79bdc76849af058952ace4830fc68
MainMemory <sonicmike2@yahoo.com>
2016-08-19 12:55:40 -0500

    Whoops

--- libmodutils/GameObject.cpp
+++ libmodutils/GameObject.cpp
@@ -2,7 +2,7 @@
 #include "GameObject.h"
 
 
-CGameObject::CGameObject(LoadObj flags, int index)
+GameObject::GameObject(LoadObj flags, int index)
 {
 	objData = LoadObject(flags, index, CallMain);
 	if (!objData)
@@ -13,9 +13,9 @@ CGameObject::CGameObject(LoadObj flags, int index)
 	}
 }
 
-CGameObject::CGameObject(int index) : CGameObject((LoadObj)0, index){}
+GameObject::GameObject(int index) : GameObject((LoadObj)0, index){}
 
-CGameObject::CGameObject(ObjectMaster *obj)
+GameObject::GameObject(ObjectMaster *obj)
 {
 	objData->MainSub = CallMain;
 	objData->DisplaySub = CallDisplay;
@@ -25,36 +25,36 @@ CGameObject::CGameObject(ObjectMaster *obj)
 }
 
 template <typename T>
-void CGameObject::Load(ObjectMaster *obj)
+void GameObject::Load(ObjectMaster *obj)
 {
 	new T(obj);
 	obj->MainSub();
 }
 
-void CGameObject::CallMain(ObjectMaster *obj)
+void GameObject::CallMain(ObjectMaster *obj)
 {
-	((CGameObject *)obj->Data2)->Main();
+	((GameObject *)obj->Data2)->Main();
 }
 
-void CGameObject::CallDisplay(ObjectMaster *obj)
+void GameObject::CallDisplay(ObjectMaster *obj)
 {
-	((CGameObject *)obj->Data2)->Display();
+	((GameObject *)obj->Data2)->Display();
 }
 
-void CGameObject::CallDelete(ObjectMaster *obj)
+void GameObject::CallDelete(ObjectMaster *obj)
 {
-	CGameObject *cgo = (CGameObject *)obj->Data2;
+	GameObject *cgo = (GameObject *)obj->Data2;
 	cgo->Delete();
 	cgo->objData = nullptr;
 	if (cgo->initFromObj)
 		delete cgo;
 }
 
-void CGameObject::Display(){}
+void GameObject::Display(){}
 
-void CGameObject::Delete(){}
+void GameObject::Delete(){}
 
-CGameObject::~CGameObject()
+GameObject::~GameObject()
 {
 	if (objData)
 	{
@@ -63,32 +63,32 @@ CGameObject::~CGameObject()
 	}
 }
 
-CGameEntity::CGameEntity(int index) :CGameObject(LoadObj_Data1, index){}
+GameEntity::GameEntity(int index) :GameObject(LoadObj_Data1, index){}
 
-EntityData1 *CGameEntity::GetData() { return objData->Data1; }
+EntityData1 *GameEntity::GetData() { return objData->Data1; }
 
-char CGameEntity::GetAction() { return GetData()->Action; }
+char GameEntity::GetAction() { return GetData()->Action; }
 
-void CGameEntity::SetAction(char action) { GetData()->Action = action; }
+void GameEntity::SetAction(char action) { GetData()->Action = action; }
 
-short CGameEntity::GetInvulnerableTime() { return GetData()->InvulnerableTime; }
+short GameEntity::GetInvulnerableTime() { return GetData()->InvulnerableTime; }
 
-void CGameEntity::SetInvulnerableTime(short time) { GetData()->InvulnerableTime = time; }
+void GameEntity::SetInvulnerableTime(short time) { GetData()->InvulnerableTime = time; }
 
-char CGameEntity::GetCharIndex() { return GetData()->CharIndex; }
+char GameEntity::GetCharIndex() { return GetData()->CharIndex; }
 
-void CGameEntity::SetCharIndex(char index) { GetData()->CharIndex = index; }
+void GameEntity::SetCharIndex(char index) { GetData()->CharIndex = index; }
 
-Rotation3 &CGameEntity::GetRotation() { return GetData()->Rotation; }
+Rotation3 &GameEntity::GetRotation() { return GetData()->Rotation; }
 
-void CGameEntity::SetRotation(Rotation3 &rotation) { GetData()->Rotation = rotation; }
+void GameEntity::SetRotation(Rotation3 &rotation) { GetData()->Rotation = rotation; }
 
-NJS_VECTOR &CGameEntity::GetPosition() { return GetData()->Position; }
+NJS_VECTOR &GameEntity::GetPosition() { return GetData()->Position; }
 
-void CGameEntity::SetPosition(NJS_VECTOR &position) { GetData()->Position = position; }
+void GameEntity::SetPosition(NJS_VECTOR &position) { GetData()->Position = position; }
 
-NJS_VECTOR &CGameEntity::GetScale() { return GetData()->Scale; }
+NJS_VECTOR &GameEntity::GetScale() { return GetData()->Scale; }
 
-void CGameEntity::SetScale(NJS_VECTOR &scale) { GetData()->Scale = scale; }
+void GameEntity::SetScale(NJS_VECTOR &scale) { GetData()->Scale = scale; }
 
-CollisionInfo *CGameEntity::GetCollisionInfo() { return GetData()->CollisionInfo; }
+CollisionInfo *GameEntity::GetCollisionInfo() { return GetData()->CollisionInfo; }
