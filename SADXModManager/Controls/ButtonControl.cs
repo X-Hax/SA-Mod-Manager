90a18f465b5a618cb5cb1ae0868e19e8fbbe0402
Egor <DankRank@users.noreply.github.com>
2017-06-28 09:14:04 +0300

    Fixed indentation, added .editorconfig

--- SADXModManager/Controls/ButtonControl.cs
+++ SADXModManager/Controls/ButtonControl.cs
@@ -3,26 +3,26 @@ using System.Windows.Forms;
 
 namespace SADXModManager.Controls
 {
-    public partial class ButtonControl : UserControl
-    {
-        public ButtonControl()
-        {
-            InitializeComponent();
-        }
+	public partial class ButtonControl : UserControl
+	{
+		public ButtonControl()
+		{
+			InitializeComponent();
+		}
 
-        public event EventHandler SetButtonClicked = delegate { };
-        public event EventHandler ClearButtonClicked = delegate { };
+		public event EventHandler SetButtonClicked = delegate { };
+		public event EventHandler ClearButtonClicked = delegate { };
 
-        public override string Text { get { return label1.Text; } set { label1.Text = value; } }
+		public override string Text { get { return label1.Text; } set { label1.Text = value; } }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            SetButtonClicked(this, EventArgs.Empty);
-        }
+		private void button1_Click(object sender, EventArgs e)
+		{
+			SetButtonClicked(this, EventArgs.Empty);
+		}
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            ClearButtonClicked(this, EventArgs.Empty);
-        }
-    }
+		private void button2_Click(object sender, EventArgs e)
+		{
+			ClearButtonClicked(this, EventArgs.Empty);
+		}
+	}
 }
