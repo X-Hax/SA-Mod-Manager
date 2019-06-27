22986cb7b22823dd93787b5623af0f6bc414f486
Michael Fadely <michaelwfadely@gmail.com>
2019-06-26 19:41:20 -0600

    don't use double for float fields, C++ style casts, style

--- SADXModLoader/ChunkSpecularFix.cpp
+++ SADXModLoader/ChunkSpecularFix.cpp
@@ -14,43 +14,44 @@ void sub_78E3D0(unsigned int ambient, unsigned int diffuse, unsigned int specula
 	specularColor.color = specular;
 
 	//v3.Diffuse.r = (double)(*(unsigned int *)&diffuse >> 24);
-	v3.Diffuse.a = (double)diffuseColor.argb.a;
-	v3.Diffuse.r = (double)diffuseColor.argb.r;
-	v3.Diffuse.g = (double)diffuseColor.argb.g;
-	v3.Diffuse.b = (double)diffuseColor.argb.b;
+	v3.Diffuse.a = static_cast<float>(diffuseColor.argb.a);
+	v3.Diffuse.r = static_cast<float>(diffuseColor.argb.r);
+	v3.Diffuse.g = static_cast<float>(diffuseColor.argb.g);
+	v3.Diffuse.b = static_cast<float>(diffuseColor.argb.b);
 
-	v3.Ambient.a = (double)ambientColor.argb.a;
-	v3.Ambient.r = (double)ambientColor.argb.r;
-	v3.Ambient.g = (double)ambientColor.argb.g;
-	v3.Ambient.b = (double)ambientColor.argb.b;
+	v3.Ambient.a = static_cast<float>(ambientColor.argb.a);
+	v3.Ambient.r = static_cast<float>(ambientColor.argb.r);
+	v3.Ambient.g = static_cast<float>(ambientColor.argb.g);
+	v3.Ambient.b = static_cast<float>(ambientColor.argb.b);
 
-	v3.Specular.a = (double)specularColor.argb.a;
-	v3.Specular.r = (double)specularColor.argb.r;
-	v3.Specular.g = (double)specularColor.argb.g;
-	v3.Specular.b = (double)specularColor.argb.b;
+	v3.Specular.a = static_cast<float>(specularColor.argb.a);
+	v3.Specular.r = static_cast<float>(specularColor.argb.r);
+	v3.Specular.g = static_cast<float>(specularColor.argb.g);
+	v3.Specular.b = static_cast<float>(specularColor.argb.b);
 
 	// divide by 255
-	v3.Ambient.a = v3.Ambient.a * 0.0039215689;
-	v3.Ambient.r = v3.Ambient.r * 0.0039215689;
-	v3.Ambient.g = v3.Ambient.g * 0.0039215689;
-	v3.Ambient.b = v3.Ambient.b * 0.0039215689;
+	v3.Ambient.a = v3.Ambient.a / 255.0f;
+	v3.Ambient.r = v3.Ambient.r / 255.0f;
+	v3.Ambient.g = v3.Ambient.g / 255.0f;
+	v3.Ambient.b = v3.Ambient.b / 255.0f;
 
-	v3.Diffuse.a = v3.Diffuse.a * 0.0039215689;
-	v3.Diffuse.r = v3.Diffuse.r * 0.0039215689;
-	v3.Diffuse.g = v3.Diffuse.g * 0.0039215689;
-	v3.Diffuse.b = v3.Diffuse.b * 0.0039215689;
+	v3.Diffuse.a = v3.Diffuse.a / 255.0f;
+	v3.Diffuse.r = v3.Diffuse.r / 255.0f;
+	v3.Diffuse.g = v3.Diffuse.g / 255.0f;
+	v3.Diffuse.b = v3.Diffuse.b / 255.0f;
 
-	v3.Specular.a = v3.Specular.a * 0.0039215689;
-	v3.Specular.r = v3.Specular.r * 0.0039215689;
-	v3.Specular.g = v3.Specular.g * 0.0039215689;
-	v3.Specular.b = v3.Specular.b * 0.0039215689;
+	v3.Specular.a = v3.Specular.a / 255.0f;
+	v3.Specular.r = v3.Specular.r / 255.0f;
+	v3.Specular.g = v3.Specular.g / 255.0f;
+	v3.Specular.b = v3.Specular.b / 255.0f;
 
 	v3.Power = exponent;
 
-	v3.Emissive.a = 0.0;
-	v3.Emissive.r = 0.0;
-	v3.Emissive.g = 0.0;
-	v3.Emissive.b = 0.0;
+	v3.Emissive.a = 0.0f;
+	v3.Emissive.r = 0.0f;
+	v3.Emissive.g = 0.0f;
+	v3.Emissive.b = 0.0f;
+
 	if (_nj_control_3d_flag_ & NJD_CONTROL_3D_CONSTANT_MATERIAL)
 	{
 		v3.Diffuse.a = _nj_constant_material_.a;
@@ -58,6 +59,7 @@ void sub_78E3D0(unsigned int ambient, unsigned int diffuse, unsigned int specula
 		v3.Diffuse.g = _nj_constant_material_.g;
 		v3.Diffuse.b = _nj_constant_material_.b;
 	}
+
 	Direct3D_Device->SetMaterial(&v3);
 	*(int*)0x389D8F4 = (int)diffuseColor.color;
 }
@@ -79,26 +81,26 @@ void __cdecl sub_791410(int* a1) //DiffuseAmbient
 
 void __cdecl sub_791430(int* a1) //Specular
 {
-	uint16_t* shrt = (uint16_t*)a1;
-	sub_78E3D0(0xFFFFFFFF, 0xFFFFFFFF, *a1, shrt[1] >> 8);
+	auto shrt = reinterpret_cast<uint16_t*>(a1);
+	sub_78E3D0(0xFFFFFFFF, 0xFFFFFFFF, *a1, static_cast<float>(shrt[1] >> 8));
 }
 
 void __cdecl sub_791450(int* a1) //DiffuseSpecular
 {
-	uint16_t* shrt = (uint16_t*)& a1[1];
-	sub_78E3D0(0xFFFFFFFF, *a1, a1[1], shrt[1] >> 8);
+	auto shrt = reinterpret_cast<uint16_t*>(&a1[1]);
+	sub_78E3D0(0xFFFFFFFF, *a1, a1[1], static_cast<float>(shrt[1] >> 8));
 }
 
 void __cdecl sub_791470(int* a1) //AmbientSpecular
 {
-	uint16_t* shrt = (uint16_t*)& a1[1];
-	sub_78E3D0(*a1, 0xFFFFFFFF, a1[1], shrt[1] >> 8);
+	auto shrt = reinterpret_cast<uint16_t*>(&a1[1]);
+	sub_78E3D0(*a1, 0xFFFFFFFF, a1[1], static_cast<float>(shrt[1] >> 8));
 }
 
 void __cdecl sub_791490(int* a1) //DiffuseAmbientSpecular
 {
-	uint16_t* shrt = (uint16_t*)& a1[2];
-	sub_78E3D0(a1[1], *a1, a1[2], shrt[1] >> 8);
+	auto shrt = reinterpret_cast<uint16_t*>(&a1[2]);
+	sub_78E3D0(a1[1], *a1, a1[2], static_cast<float>(shrt[1] >> 8));
 }
 
 void ChunkSpecularFix_Init()
