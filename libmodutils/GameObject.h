a675e53fa1296a6fbb3c1518b549863c78369599
Michael Fadely <michaelwfadely@gmail.com>
2019-06-30 15:22:07 -0600

    GameObject

--- libmodutils/GameObject.h
+++ libmodutils/GameObject.h
@@ -1,29 +1,32 @@
 #pragma once
 #include "SADXModLoader.h"
+
 class GameObject
 {
 private:
-	static void CallMain(ObjectMaster *obj);
-	static void CallDisplay(ObjectMaster *obj);
-	static void CallDelete(ObjectMaster *obj);
+	static void CallMain(ObjectMaster* obj);
+	static void CallDisplay(ObjectMaster* obj);
+	static void CallDelete(ObjectMaster* obj);
 	bool initFromObj;
 protected:
-	ObjectMaster *objData;
+	ObjectMaster* objData;
 	GameObject(LoadObj flags, int index);
-	explicit GameObject(ObjectMaster *obj);
+	explicit GameObject(ObjectMaster* obj);
 	virtual void Delete();
 public:
 	explicit GameObject(int index);
 	~GameObject();
 	virtual void Main() = 0;
 	virtual void Display();
+
 	template <typename T>
-	static void Load(ObjectMaster *obj)
+	static void Load(ObjectMaster* obj)
 	{
-		T *t = new T(obj);
+		T* t = new T(obj);
 		t->Main();
 	}
-	SETDataUnion GetSETData();
+
+	SETDataUnion GetSETData() const;
 	// MSVC-specific property emulation.
 #ifdef _MSC_VER
 	__declspec(property(get = GetSETData)) SETDataUnion SETData;
@@ -33,30 +36,30 @@ public:
 class GameEntity : GameObject
 {
 protected:
-	EntityData1 *GetData();
+	EntityData1* GetData() const;
 public:
 	explicit GameEntity(int index);
-	char GetAction();
-	void SetAction(char action);
-	short GetInvulnerableTime();
-	void SetInvulnerableTime(short time);
-	char GetCharIndex();
-	void SetCharIndex(char index);
-	Rotation3 &GetRotation();
-	void SetRotation(Rotation3 &rotation);
-	NJS_VECTOR &GetPosition();
-	void SetPosition(NJS_VECTOR &position);
-	NJS_VECTOR &GetScale();
-	void SetScale(NJS_VECTOR &scale);
-	CollisionInfo *GetCollisionInfo();
+	char GetAction() const;
+	void SetAction(char action) const;
+	short GetInvulnerableTime() const;
+	void SetInvulnerableTime(short time) const;
+	char GetCharIndex() const;
+	void SetCharIndex(char index) const;
+	Rotation3& GetRotation() const;
+	void SetRotation(Rotation3& rotation) const;
+	NJS_VECTOR& GetPosition() const;
+	void SetPosition(NJS_VECTOR& position) const;
+	NJS_VECTOR& GetScale() const;
+	void SetScale(NJS_VECTOR& scale) const;
+	CollisionInfo* GetCollisionInfo() const;
 	// MSVC-specific property emulation.
 #ifdef _MSC_VER
 	__declspec(property(get = GetAction, put = SetAction)) char Action;
 	__declspec(property(get = GetInvulnerableTime, put = SetInvulnerableTime)) short InvulnerableTime;
 	__declspec(property(get = GetCharIndex, put = SetCharIndex)) char CharIndex;
-	__declspec(property(get = GetRotation, put = SetRotation)) Rotation3 &Rotation;
-	__declspec(property(get = GetPosition, put = SetPosition)) NJS_VECTOR &Position;
-	__declspec(property(get = GetScale, put = SetScale)) NJS_VECTOR &Scale;
-	__declspec(property(get = GetCollisionInfo)) CollisionInfo *CollisionInfo;
+	__declspec(property(get = GetRotation, put = SetRotation)) Rotation3& Rotation;
+	__declspec(property(get = GetPosition, put = SetPosition)) NJS_VECTOR& Position;
+	__declspec(property(get = GetScale, put = SetScale)) NJS_VECTOR& Scale;
+	__declspec(property(get = GetCollisionInfo)) CollisionInfo* CollisionInfo;
 #endif
 };
