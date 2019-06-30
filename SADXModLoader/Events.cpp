b55b6b0c6d2aac053ec3aa50b1897b0a7761b4e5
Michael Fadely <michaelwfadely@gmail.com>
2019-06-30 15:04:23 -0600

    Events

--- SADXModLoader/Events.cpp
+++ SADXModLoader/Events.cpp
@@ -2,30 +2,32 @@
 #include "Trampoline.h"
 #include "Events.h"
 
-std::vector<ModEvent> modFrameEvents;
-std::vector<ModEvent> modInputEvents;
-std::vector<ModEvent> modControlEvents;
-std::vector<ModEvent> modExitEvents;
+std::vector<ModEvent>         modFrameEvents;
+std::vector<ModEvent>         modInputEvents;
+std::vector<ModEvent>         modControlEvents;
+std::vector<ModEvent>         modExitEvents;
 std::vector<TextureLoadEvent> modCustomTextureLoadEvents;
-std::vector<ModEvent> modRenderDeviceLost;
-std::vector<ModEvent> modRenderDeviceReset;
-std::vector<ModEvent> onRenderSceneEnd;
-std::vector<ModEvent> onRenderSceneStart;
+std::vector<ModEvent>         modRenderDeviceLost;
+std::vector<ModEvent>         modRenderDeviceReset;
+std::vector<ModEvent>         onRenderSceneEnd;
+std::vector<ModEvent>         onRenderSceneStart;
 
-Trampoline exitDetour((size_t)0x0064672F, 0x00646736, OnExit);
+Trampoline exitDetour(0x0064672F, 0x00646736, OnExit);
 
 /**
-* Registers an event to the specified event list.
-* @param eventList The event list to add to.
-* @param module The module for the mod DLL.
-* @param name The name of the exported function from the module (i.e OnFrame)
-*/
+ * Registers an event to the specified event list.
+ * @param eventList The event list to add to.
+ * @param module The module for the mod DLL.
+ * @param name The name of the exported function from the module (i.e OnFrame)
+ */
 void RegisterEvent(std::vector<ModEvent>& eventList, HMODULE module, const char* name)
 {
-	const ModEvent modEvent = (const ModEvent)GetProcAddress(module, name);
+	const auto modEvent = reinterpret_cast<const ModEvent>(GetProcAddress(module, name));
 
 	if (modEvent != nullptr)
+	{
 		eventList.push_back(modEvent);
+	}
 }
 
 DataPointer(short, word_3B2C464, 0x3B2C464);
