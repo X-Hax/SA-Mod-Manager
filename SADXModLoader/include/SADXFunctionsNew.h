01eae425d53ca87d32916c026fe389bd86902394
PkR <pkr@pkr.pkr>
2020-10-16 14:18:54 +0900

    Add more stuff from symbols

--- SADXModLoader/include/SADXFunctionsNew.h
+++ SADXModLoader/include/SADXFunctionsNew.h
@@ -24,5 +24,12 @@ typedef Uint32 NJD_TEXATTR;
 typedef bool _BOOL1;
 
 // SADX Functions
+FunctionPointer(task*, CreateChildTask, (unsigned __int16 im, void(__cdecl* exec)(task*), task* tp), 0x40B940);
+FunctionPointer(task*, CreateElementalTask, (unsigned __int16 im, int level, void(__cdecl* exec)(task*)), 0x40B860);
+FunctionPointer(void, DestroyTask, (task* tp), 0x40B570);
+FunctionPointer(void, FreeTask, (task* tp), 0x40B6C0);
+FunctionPointer(void, RegisterCollisionEntry, (int slAttribute, task* pTask, obj* pObject), 0x43B2E0);
+FunctionPointer(void, WithdrawCollisionEntry, (task* pTask, obj* pObject), 0x43B380);
+FunctionPointer(void, B_Destructor, (task* tp), 0x59DBF0);
 
 #endif /* SADXMODLOADER_SADXFUNCTIONSNEW_H */
--- SADXModLoader/include/SADXStructsNew.h
+++ SADXModLoader/include/SADXStructsNew.h
@@ -321,6 +321,757 @@ struct _SEcallbuf
 	int banknum;
 };
 
+struct _OBJ_LANDENTRY
+{
+	float xCenter;
+	float yCenter;
+	float zCenter;
+	float xWidth;
+	float yWidth;
+	float zWidth;
+	obj* pObject;
+	int blockbit;
+	int slAttribute;
+};
+
+struct _OBJ_MOTLANDENTRY
+{
+	float fFrame;
+	float fStep;
+	float fMaxFrame;
+	obj* pObject;
+	NJS_MOTION* pMotion;
+	NJS_TEXLIST* pTexList;
+};
+
+struct _OBJ_LANDTABLE
+{
+	__int16 ssCount;
+	__int16 ssMotCount;
+	__int16 ssAttribute;
+	__int16 ssLoadFlag;
+	float fFarClipping;
+	_OBJ_LANDENTRY* pLandEntry;
+	_OBJ_MOTLANDENTRY* pMotLandEntry;
+	char* pPvmFileName;
+	NJS_TEXLIST* pTexList;
+	char* pBinaryFileName;
+	void* pLoadBinary;
+};
+
+struct TEX_PVMTABLE
+{
+	char* pname;
+	NJS_TEXLIST* ptexlist;
+};
+
+struct ADVERTISE_WORK
+{
+	char flags[8];
+	char SelectedVmsNo;
+	char SelectedFileNo;
+	char MainMenuSelectedMode;
+	char CharSelCsr;
+	char TitleCnt;
+	char PlayerChar;
+	char Stage;
+	char Act;
+	unsigned __int16 GamePlayerNumber;
+};
+
+struct PDS_PERIPHERALINFO
+{
+	unsigned int type;
+	unsigned int reserved[3];
+	char is_root;
+	char area_code;
+	char connector_dir[2];
+	char product_name[32];
+	char license[64];
+	unsigned __int16 stdby_pow;
+	unsigned __int16 max_pow;
+};
+
+struct PDS_PERIPHERAL
+{
+	unsigned int id;
+	unsigned int support;
+	unsigned int on;
+	unsigned int off;
+	unsigned int press;
+	unsigned int release;
+	unsigned __int16 r;
+	unsigned __int16 l;
+	__int16 x1;
+	__int16 y1;
+	__int16 x2;
+	__int16 y2;
+	char* name;
+	void* extend;
+	unsigned int old;
+	PDS_PERIPHERALINFO* info;
+};
+
+struct PDS_KEYBOARDINFO
+{
+	char lang;
+	char type;
+	char led;
+	char led_ctrl;
+};
+
+struct PDS_KEYBOARD
+{
+	char ctrl;
+	char led;
+	char key[6];
+	PDS_KEYBOARDINFO* info;
+};
+
+struct PDS_MOUSE_INFO
+{
+	char reserved;
+	char button;
+	char axis;
+};
+
+struct PDS_MOUSE_BUTTON
+{
+	unsigned int on;
+	unsigned int off;
+	unsigned int press;
+	unsigned int release;
+	unsigned int old;
+	char connect;
+	char opt;
+	char reserved[2];
+};
+
+union taskwork_subs
+{
+	char b[4];
+	__int16 w[2];
+	int l;
+	float f;
+	void* ptr;
+};
+
+struct xssunit
+{
+	int findflag;
+	int objatt;
+	int angx;
+	int angz;
+	float onpos;
+	NJS_POINT3 normal;
+};
+
+struct xyyzzxsdwstr
+{
+	NJS_POINT3 pos;
+	xssunit hit[6];
+};
+
+struct enemywk
+{
+	taskwork_subs sub;
+	NJS_POINT3 velo;
+	NJS_POINT3 acc;
+	Angle3 ang_aim;
+	Angle3 ang_spd;
+	float radius;
+	float height;
+	float weight;
+	task* lock_tp;
+	void(__cdecl* dest_org)(task*);
+	char pnum;
+	char old_mode;
+	char old_smode;
+	__int16 aim_num;
+	unsigned __int16 flag;
+	NJS_POINT3 home;
+	NJS_POINT3 aim;
+	NJS_POINT3 pre;
+	NJS_POINT3 force;
+	NJS_POINT3 norm;
+	NJS_ARGB argb;
+	NJS_POINT3 colli_center;
+	float colli_top;
+	float colli_radius;
+	float colli_bottom;
+	float cliff_height;
+	float bound_side;
+	float bound_floor;
+	float bound_friction;
+	float bound_ceiling;
+	int bound_add_angle;
+	float shadow_scl;
+	float shadow_scl_ratio;
+	xyyzzxsdwstr shadow;
+	float buyoscale;
+	int angx_spd;
+	int angy_spd;
+	int angz_spd;
+	int sub_angle[8];
+	int aim_angle;
+	int view_angle;
+	float view_range;
+	float hear_range;
+	float nframe;
+	float pframe;
+	float lframe;
+	float link_ratio;
+	float link_spd;
+	NJS_ACTION* actp;
+	NJS_ACTION* linkactp;
+	float mat[16];
+};
+
+struct NpcMessageTable
+{
+	const __int16* code;
+	const char** message;
+};
+
+struct NpcMessageTable2
+{
+	__int16* code;
+	char** message;
+};
+
+struct SPLINE_DATA
+{
+	void* src;
+	int data_size;
+	char* fmt;
+	float rate;
+	void* dst;
+	int spline_type;
+	int variety;
+};
+
+struct _OBJ_CAMERAPARAM
+{
+	unsigned __int16 xCamAng;
+	unsigned __int16 yCamAng;
+	float xCamPos;
+	float yCamPos;
+	float zCamPos;
+	float xDirPos;
+	float yDirPos;
+	float zDirPos;
+	float fDistance;
+	unsigned int ulTimer;
+};
+
+struct sSplineData
+{
+	unsigned __int16 InitPoint;
+	unsigned __int16 NumPos;
+	NJS_POINT3* Points;
+};
+
+struct uv_info
+{
+	NJS_COLOR* uv_list;
+	int uv_num;
+};
+
+struct neon_info
+{
+	uv_info* info;
+	__int16 info_num;
+	__int16 type;
+	__int16 uv_spd;
+	__int16 wait_max;
+	__int16 timer;
+	__int16 uv;
+};
+
+struct HintMessageString
+{
+	const char* message;
+	int wait;
+};
+
+struct HintMessageTable
+{
+	const __int16* code;
+	HintMessageString* message;
+};
+
+struct PL_ACTION
+{
+	NJS_ACTION* actptr;
+	char objnum;
+	char mtnmode;
+	unsigned __int16 next;
+	float frame;
+	float racio;
+};
+
+struct PL_JOIN_VERTEX
+{
+	obj* objptr;
+	obj* srcobj;
+	obj* dstobj;
+	char numVertex;
+	char inpmode;
+	char srcdepth;
+	char dstdepth;
+	NJS_POINT3* org;
+	unsigned __int16* pnum;
+};
+
+struct bosswk
+{
+	taskwork_subs work;
+	__int16 mtnmode;
+	__int16 jvmode;
+	unsigned __int16 req_action;
+	unsigned __int16 action;
+	unsigned __int16 lastaction;
+	unsigned __int16 action_tm;
+	float nframe;
+	float xacc;
+	float yacc;
+	float zacc;
+	float xspd;
+	float yspd;
+	float zspd;
+	PL_ACTION* plactptr;
+	PL_JOIN_VERTEX* pljvptr;
+	NJS_ACTION* actwkptr;
+};
+
+struct chaoswk
+{
+	bosswk bwk;
+	int generaltimer;
+	char txchg_tmr;
+	char txchg_num;
+	char form;
+	char sp_mode;
+	char colli_mode;
+	char headsts;
+	char camera_mode;
+	__int16 etcflag;
+	__int16 dispflag;
+	__int16 morph_time;
+	NJS_POINT3 sp_scale;
+	NJS_POINT3 tgtpos_s;
+	NJS_POINT3 tgtpos;
+	float HitPoint;
+	float attack_dist;
+	int attack_zang;
+	int attack_yang;
+	NJS_ARGB argb_ofs[2];
+	float morph_param;
+	__int16 attack_tmr;
+	char barrier_ptno;
+	char barrier_tmr;
+	float barrier_a;
+	NJS_TEXLIST* texlist;
+	float ground_y;
+	void* morph_wp;
+	float param;
+	unsigned int old_texaddr;
+};
+
+struct Chaos0Work
+{
+	NJS_POINT3 dest_pos;
+	NJS_POINT3 under_pos;
+	NJS_POINT3 velocity;
+	float phai;
+	float dphai;
+	float jumph;
+	int frame;
+	float dmot;
+	int jump_mode;
+	float mot_spd;
+	int cng_int;
+	float human_y;
+	float human_dy;
+	NJS_POINT3 manju_base_scale;
+	NJS_POINT3 manju_dscale;
+	NJS_POINT3 manju_scale;
+	float manju_alpha;
+	float manju_dalpha;
+	int manju_anim_cnt;
+};
+
+struct Chaos1Work
+{
+	int cng_int;
+	float mot_spd;
+	float human_y;
+	float human_dy;
+	NJS_POINT3 manju_base_scale;
+	NJS_POINT3 manju_dscale;
+	NJS_POINT3 manju_scale;
+	float manju_alpha;
+	float manju_dalpha;
+};
+
+struct Chaos2Work
+{
+	NJS_POINT3 dest_pos;
+	NJS_POINT3 velocity;
+	int frame;
+	int cng_int;
+	float mot_spd;
+	float mot_cnt;
+	float human_y;
+	float human_dy;
+	NJS_POINT3 manju_base_scale;
+	NJS_POINT3 manju_dscale;
+	NJS_POINT3 manju_scale;
+	float manju_alpha;
+	float manju_dalpha;
+	int manju_anim_cnt;
+};
+
+struct Chaos4Work
+{
+	NJS_POINT3 dest_pos;
+	NJS_POINT3 velocity;
+	int frame;
+	int cng_int;
+	float mot_spd;
+	float human_y;
+	float human_dy;
+	NJS_POINT3 manju_base_scale;
+	NJS_POINT3 manju_dscale;
+	NJS_POINT3 manju_scale;
+	float manju_alpha;
+	float manju_dalpha;
+	int manju_anim_cnt;
+};
+
+struct pathinfo
+{
+	int slangx;
+	int slangz;
+	int slangax;
+	int slangaz;
+	float onpathpos;
+	NJS_POINT3 pos;
+	float xpos;
+	float ypos;
+	float zpos;
+	NJS_POINT3 normal;
+	NJS_POINT3 normala;
+	NJS_POINT3 front;
+};
+
+struct motionwk2
+{
+	taskwork_subs work;
+	NJS_POINT3 spd;
+	NJS_POINT3 acc;
+	Angle3 ang_aim;
+	Angle3 ang_spd;
+	float force;
+	float accel;
+	float frict;
+};
+
+struct XYZ_SHADOW_WORK
+{
+	NJS_POINT3 pos;
+	xssunit hit[6];
+	xssunit pre_hit[6];
+};
+
+struct MOVE_WORK
+{
+	taskwork_subs work;
+	NJS_POINT3 Velo;
+	NJS_POINT3 Acc;
+	Angle3 AimAng;
+	Angle3 RotSpd;
+	float rad;
+	float height;
+	float weight;
+	unsigned __int16 Flag;
+	unsigned __int16 Timer;
+	float Spd;
+	float Gravity;
+	int ViewAngle;
+	float ViewRange;
+	NJS_POINT3 AimPos;
+	NJS_POINT3 PrePos;
+	NJS_POINT3 HomePos;
+	Angle3 HomeAng;
+	Angle3 Phase;
+	NJS_LINE FrontWall;
+	NJS_POINT3 Offset;
+	float Top;
+	float Side;
+	float Bottom;
+	float CliffHeight;
+	float BoundSide;
+	float BoundFloor;
+	float BoundCeiling;
+	float BoundFriction;
+	float TopY;
+	float BottomY;
+	float WaterY;
+	XYZ_SHADOW_WORK Shadow;
+};
+
+struct al_entry_work
+{
+	unsigned __int16 category;
+	unsigned __int16 num;
+	unsigned __int16 kind;
+	unsigned __int16 flag;
+	void* pSaveInfo;
+	int CommuID;
+	NJS_POINT3 pos;
+	Angle3 ang;
+	float radius;
+	float offset;
+	float CamDist;
+	__int16 command;
+	__int16 command_value;
+	__int16 state;
+	task* tp;
+	al_entry_work* pCommu;
+	al_entry_work* pLockOn;
+};
+
+struct __declspec(align(2)) AL_BODY_INFO
+{
+	float HPos;
+	float VPos;
+	float APos;
+	float AimHPos;
+	float AimVPos;
+	float AimAPos;
+	float growth;
+	float EyePos;
+	float EyeScl;
+	float EyeRatio;
+	char EyeColor;
+	char DefaultEyeNum;
+	char DefaultMouthNum;
+	char HonbuNum;
+	char HonbuColorNum;
+	char ObakeHead;
+	char ObakeBody;
+	char MedalNum;
+	char ColorNum;
+	char NonTex;
+	char JewelNum;
+	char MultiNum;
+	char EggColor;
+	char FormNum;
+	char FormSubNum;
+};
+
+struct AL_TIME
+{
+	char minute;
+	char second;
+	char frame;
+};
+
+
+struct cnkobj
+{
+	unsigned int evalflags;
+	NJS_CNK_MODEL* model;
+	float pos[3];
+	int ang[3];
+	float scl[3];
+	cnkobj* child;
+	cnkobj* sibling;
+};
+
+struct AL_BUYO_PARAM
+{
+	float Spring1;
+	float Spring2;
+	float Friction1;
+	float Friction2;
+	float MaxDiff;
+	float MaxSpd;
+	float Weight1;
+	float Weight2;
+	float Weight3;
+	NJS_POINT3 Center;
+};
+
+struct AL_RACE_PERSONAL_INFO
+{
+	AL_TIME PersonalRecord[10];
+	char nbWin[10];
+	unsigned __int16 MedalFlag;
+};
+
+struct AL_RACE_PERSONAL_INFO_DX
+{
+	AL_TIME PersonalRecord[5];
+};
+
+struct TMP_PARAM
+{
+	char Exp[8];
+	char Abl[8];
+	char Lev[8];
+	unsigned __int16 Skill[8];
+};
+
+struct CNK_VN_VERTEX
+{
+	NJS_POINT3 Pos;
+	NJS_POINT3 Normal;
+};
+
+struct AL_VERTEX_INFO
+{
+	NJS_POINT3 Velo;
+	float Weight;
+	float Spring;
+	float Friction;
+};
+
+struct al_model
+{
+	int* VList;
+	__int16* PList;
+	NJS_POINT3 Center;
+	float Radius;
+	__int16 OrgTexId[4];
+	int PListSize;
+	int nbVertex;
+	CNK_VN_VERTEX* pVertex;
+	NJS_POINT3* pOrgVertexPos;
+	NJS_POINT3* pOrgVertexNormal;
+	AL_VERTEX_INFO* pVertexInfo;
+};
+
+struct al_object
+{
+	unsigned int EvalFlags;
+	al_model* pModel;
+	float Pos[3];
+	int Ang[3];
+	float Scl[3];
+	al_object* pChild;
+	al_object* pSibling;
+	al_object* pParent;
+	NJS_POINT3 diff;
+	NJS_POINT3 GlobalAimPos;
+	NJS_POINT3 OrgPos;
+	NJS_POINT3 Velo;
+	NJS_POINT3 OrgAng;
+	NJS_POINT3 AngSpd;
+	float Weight;
+	float Spring;
+	float Friction;
+	int NoBuyoFlag;
+	int CalcBuyoPosFlag;
+	cnkobj* pPartsObject;
+	cnkobj* pItemObject;
+	NJS_TEXLIST* pItemTexlist;
+	float ItemScale;
+	int ItemActiveFlag;
+	int ItemOffsetFlag;
+	NJS_POINT3 ItemOffsetPos;
+	Angle3 ItemOffsetAng;
+	float(*pOldMatrix)[16];
+	AL_BUYO_PARAM* pBuyoParam;
+	void* DisplayList;
+};
+
+struct AL_FACE_CTRL
+{
+	int EyeTimer;
+	__int16 EyeColorNum;
+	__int16 EyeCurrNum;
+	__int16 EyeDefaultNum;
+	int MouthTimer;
+	__int16 MouthCurrNum;
+	__int16 MouthDefaultNum;
+	float EyePosX;
+	float EyePosY;
+	float EyeSclX;
+	float EyeSclY;
+	unsigned int Flag;
+	al_object* pEyeObject[2];
+	al_object* pMouthObject;
+	int EyeLidBlinkMode;
+	int EyeLidBlinkTimer;
+	int EyeLidBlinkAng;
+	int EyeLidExpressionMode;
+	int EyeLidExpressionTimer;
+	int EyeLidExpressionDefaultCloseAng;
+	int EyeLidExpressionCurrCloseAng;
+	int EyeLidExpressionAimCloseAng;
+	int EyeLidExpressionDefaultSlopeAng;
+	int EyeLidExpressionCurrSlopeAng;
+	int EyeLidExpressionAimSlopeAng;
+};
+
+struct AL_KNOWLEDGE_OTHER
+{
+	char like;
+};
+
+struct AL_EMOTION
+{
+	unsigned __int16 Flag;
+	unsigned __int16 MoodTimer;
+	unsigned __int16 IllTimer;
+	unsigned __int16 Timer;
+	char Mood[8];
+	unsigned __int16 State[11];
+	char Personality[13];
+	char Taste;
+	char Tv;
+	char Music;
+	char IllState[6];
+};
+
+struct al_perception_link
+{
+	__int16 info[4];
+	float dist;
+	int InSightFlag;
+	int HearFlag;
+	int SmellFlag;
+	al_entry_work* pEntry;
+};
+
+struct AL_PERCEPTION_INFO
+{
+	unsigned __int16 nbPerception;
+	int InSightFlag;
+	int HeardFlag;
+	int SmellFlag;
+	float NearestDist;
+	__int16 NearestNum;
+	al_perception_link list[32];
+};
+
+struct AL_PERCEPTION
+{
+	float SightRange;
+	int SightAngle;
+	int SightAngleHalf;
+	float HearRange;
+	float SmellRange;
+	AL_PERCEPTION_INFO Player;
+	AL_PERCEPTION_INFO Chao;
+	AL_PERCEPTION_INFO Fruit;
+	AL_PERCEPTION_INFO Tree;
+	AL_PERCEPTION_INFO Toy;
+	AL_PERCEPTION_INFO Sound;
+};
+
 #pragma pack(pop)
 
 #endif /* SADXMODLOADER_SADXSTRUCTSNEW_H */
