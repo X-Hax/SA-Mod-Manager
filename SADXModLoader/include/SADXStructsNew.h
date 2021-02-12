44860b84f12faaf391167331f5f24efb19545d9d
PkR <pkr@pkr.pkr>
2021-02-12 17:54:43 +0900

    Add player_parameter to new SADX structs

--- SADXModLoader/include/SADXStructsNew.h
+++ SADXModLoader/include/SADXStructsNew.h
@@ -1115,6 +1115,43 @@ struct LE_PALIGHT_ENV
 	NJS_RGB spe2;
 };
 
+struct player_parameter
+{
+	int jump2_timer;
+	float pos_error;
+	float lim_h_spd;
+	float lim_v_spd;
+	float max_x_spd;
+	float max_psh_spd;
+	float jmp_y_spd;
+	float nocon_speed;
+	float slide_speed;
+	float jog_speed;
+	float run_speed;
+	float rush_speed;
+	float crash_speed;
+	float dash_speed;
+	float jmp_addit;
+	float run_accel;
+	float air_accel;
+	float slow_down;
+	float run_break;
+	float air_break;
+	float air_resist_air;
+	float air_resist;
+	float air_resist_y;
+	float air_resist_z;
+	float grd_frict;
+	float grd_frict_z;
+	float lim_frict;
+	float rat_bound;
+	float rad;
+	float height;
+	float weight;
+	float eyes_height;
+	float center_height;
+};
+
 #pragma pack(pop)
 
 #endif /* SADXMODLOADER_SADXSTRUCTSNEW_H */
