68ca8cae98ff63b9f1f17fadf9a54571d27f6723
Sora <sora.x@hotmail.fr>
2021-09-26 15:13:07 +0200

    Added some Cutscenes functions and struct

--- SADXModLoader/include/SADXStructsNew.h
+++ SADXModLoader/include/SADXStructsNew.h
@@ -1310,6 +1310,36 @@ struct CHS_BILL_INFO
 	int TexNum;
 };
 
+struct PADREC_DATA
+{
+	BYTE on;
+	BYTE angle;
+	BYTE stroke;
+};
+
+struct PADREC_DATA_TAG
+{
+	PADREC_DATA** data;
+	int size;
+};
+
+struct cpathtbl
+{
+	__int16 roty;
+	__int16 rotx;
+	__int16 rotz;
+	float camx;
+	float camy;
+	float camz;
+};
+
+struct cpathtag
+{
+	unsigned __int8 type;
+	float points;
+	cpathtbl* tblhead;
+};
+
 #pragma pack(pop)
 
 #endif /* SADXMODLOADER_SADXSTRUCTSNEW_H */
