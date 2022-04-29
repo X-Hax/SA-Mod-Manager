7c2a87568390adcf1b16f68f4287a8652967a154
kellsnc <kellspam@outlook.fr>
2022-04-29 18:53:13 +0200

    SkyChase: minor adjustements

--- SADXModLoader/SkyChaseFixes.cpp
+++ SADXModLoader/SkyChaseFixes.cpp
@@ -6,15 +6,11 @@ static float HorizontalResolution_float = 640.0f;
 static float VerticalResolution_float = 480.0f;
 static float VerticalResolutionHalf_float = 240.0f;
 
-static float float_one = 1.0f;
-static float tornado_speed = 1.0f;
+static const float float_one = 1.0f;
 static float target_collision_size = 1.0f;
 static float target_speed_multiplier = 2.0f;
 
-static double SkyChaseSkyRotationMultiplier = -0.5f;
-
-static const int  hack_int = 0x39A1877F;
-static const auto hack_flt = *reinterpret_cast<const float*>(&hack_int);
+static const double SkyRotationMultiplier = -0.5;
 
 template <typename T>
 T clamp(T value, T low, T high)
@@ -32,9 +28,63 @@ T clamp(T value, T low, T high)
 	return value;
 }
 
-static void TornadoTarget_MoveTargetWithinBounds_asm();
-static void __cdecl TornadoTarget_CalculateCenterPoint_r(ObjectMaster* a1);
-static void __cdecl TornadoFixTargetSpriteScale(NJS_SPRITE* sp, Int n, Float pri, NJD_SPRITE attr);
+static void __cdecl TornadoTarget_MoveTargetWithinBounds_r(task* tp)
+{
+	auto pos = &tp->twp->pos;
+
+	const float m = min(VerticalStretch, HorizontalStretch);
+	const float move_speed = m * 0.000308093f;
+
+	// 640x480 - 160x160 margin
+	const float w = 480.0f * m;
+	const float h = 320.0f * m;
+
+	const float left = (HorizontalResolution_float - w) / 2.0f;
+	const float top = (VerticalResolution_float - h) / 2.0f;
+	const float right = left + w;
+	const float bottom = top + h;
+
+	const float x = (static_cast<float>(static_cast<int>(Controllers[0].LeftStickX) << 8) * move_speed) + pos->x;
+	const float y = (static_cast<float>(static_cast<int>(Controllers[0].LeftStickY) << 8) * move_speed) + pos->y;
+
+	pos->x = clamp(x, left, right);
+	pos->y = clamp(y, top, bottom);
+}
+
+static void __declspec(naked) TornadoTarget_MoveTargetWithinBounds_asm()
+{
+	__asm
+	{
+		push eax
+		call TornadoTarget_MoveTargetWithinBounds_r
+		pop eax
+		retn
+	}
+}
+
+static void __cdecl TornadoTarget_CalculateCenterPoint_r(task* tp)
+{
+	auto pos = &tp->twp->pos;
+	auto parent_pos = &tp->ptp->twp->pos;
+
+	pos->x = parent_pos->x - _nj_screen_.cx;
+	pos->y = parent_pos->y - _nj_screen_.cy;
+	pos->z = 1000.0f * min(VerticalStretch, HorizontalStretch);
+
+	njPushMatrix(nullptr);
+	njInvertMatrix(nullptr);
+	njCalcPoint(nullptr, pos, pos);
+	njPopMatrixEx();
+}
+
+static void __cdecl TornadoFixTargetSpriteScale(NJS_SPRITE* sp, Int n, Float pri, NJD_SPRITE attr)
+{
+	if (!IsGamePaused())
+	{
+		sp->sy = sp->sy * min(VerticalStretch, HorizontalStretch);
+		njDrawSprite2D_ForcePriority(sp, n, pri, attr);
+	}
+}
 
 void SkyChaseFix_UpdateBounds()
 {
@@ -59,9 +109,9 @@ void SkyChaseFix_Init()
 
 	WriteJump(reinterpret_cast<void*>(0x00628970), TornadoTarget_MoveTargetWithinBounds_asm);
 
-	WriteData(reinterpret_cast<float**>(0x00627F4D), &tornado_speed); // Tornado Speed (always 1)
-	WriteData(reinterpret_cast<float**>(0x00627F60), &float_one);     // Horizontal limit
-	WriteData(reinterpret_cast<float**>(0x00627F72), &float_one);     // Vertical limit
+	WriteData(reinterpret_cast<const float**>(0x00627F4D), &float_one); // Tornado Speed (always 1)
+	WriteData(reinterpret_cast<const float**>(0x00627F60), &float_one); // Horizontal limit
+	WriteData(reinterpret_cast<const float**>(0x00627F72), &float_one); // Vertical limit
 
 	WriteJump(reinterpret_cast<void*>(0x628D50), TornadoTarget_CalculateCenterPoint_r); // Calculate center for bullets
 
@@ -74,67 +124,6 @@ void SkyChaseFix_Init()
 	WriteData(reinterpret_cast<float**>(0x00629472), &target_speed_multiplier); // Target speed
 
 	// Visual stuff
-	WriteData(reinterpret_cast<double**>(0x00627D14), &SkyChaseSkyRotationMultiplier);
+	WriteData(reinterpret_cast<const double**>(0x00627D14), &SkyRotationMultiplier);
 	WriteCall(reinterpret_cast<void*>(0x00628E4D), TornadoFixTargetSpriteScale);
-}
-
-static void __cdecl TornadoTarget_MoveTargetWithinBounds_r(ObjectMaster* a1)
-{
-	EntityData1* data1 = a1->Data1;
-
-	const float m = min(VerticalStretch, HorizontalStretch);
-	const float move_speed = m * hack_flt;
-
-	// 640x480 - 160x160 margin
-	const auto w = 480.0f * m;
-	const auto h = 320.0f * m;
-
-	float x = (static_cast<float>(static_cast<int>(Controllers[0].LeftStickX) << 8) * move_speed) + data1->Position.x;
-	float y = (static_cast<float>(static_cast<int>(Controllers[0].LeftStickY) << 8) * move_speed) + data1->Position.y;
-
-	const float left = (HorizontalResolution_float - w) / 2.0f;
-	const float top = (VerticalResolution_float - h) / 2.0f;
-	const float right = left + w;
-	const float bottom = top + h;
-
-	x = clamp(x, left, right);
-	y = clamp(y, top, bottom);
-
-	data1->Position.x = x;
-	data1->Position.y = y;
-}
-
-static void __declspec(naked) TornadoTarget_MoveTargetWithinBounds_asm()
-{
-	__asm
-	{
-		push eax
-		call TornadoTarget_MoveTargetWithinBounds_r
-		pop eax
-		retn
-	}
-}
-
-static void __cdecl TornadoTarget_CalculateCenterPoint_r(ObjectMaster* a1)
-{
-	EntityData1* parent_data1 = a1->Parent->Data1;
-	NJS_VECTOR* position = &a1->Data1->Position;
-
-	position->x = parent_data1->Position.x - _nj_screen_.cx;
-	position->y = parent_data1->Position.y - _nj_screen_.cy;
-	position->z = 1000.0f * min(VerticalStretch, HorizontalStretch);
-
-	njPushMatrix(nullptr);
-	njInvertMatrix(nullptr);
-	njCalcPoint(nullptr, position, position);
-	njPopMatrix(1u);
-}
-
-static void __cdecl TornadoFixTargetSpriteScale(NJS_SPRITE* sp, Int n, Float pri, NJD_SPRITE attr)
-{
-	if (!IsGamePaused())
-	{
-		sp->sy = sp->sy * min(VerticalStretch, HorizontalStretch);
-		njDrawSprite2D_ForcePriority(sp, n, pri, attr);
-	}
 }
