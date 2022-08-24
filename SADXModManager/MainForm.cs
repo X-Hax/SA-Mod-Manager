ad18177c6d66392cad26af7feceda16716357a6f
Sora <sora.x@hotmail.fr>
2022-08-25 00:17:32 +0200

    small clean up

--- SADXModManager/MainForm.cs
+++ SADXModManager/MainForm.cs
@@ -2535,22 +2535,22 @@ namespace SADXModManager
 
 		private void Load_WindowUserSettings()
 		{
-			if (Properties.Settings.Default.Maximised)
+			if (Settings.Default.Maximised)
 			{
-				Location = Properties.Settings.Default.Location;
+				Location = Settings.Default.Location;
 				WindowState = FormWindowState.Maximized;
-				Size = Properties.Settings.Default.Size;
+				Size = Settings.Default.Size;
 			}
-			else if (Properties.Settings.Default.Minimised)
+			else if (Settings.Default.Minimised)
 			{
-				Location = Properties.Settings.Default.Location;
+				Location = Settings.Default.Location;
 				WindowState = FormWindowState.Minimized;
-				Size = Properties.Settings.Default.Size;
+				Size = Settings.Default.Size;
 			}
 			else
 			{
-				Location = Properties.Settings.Default.Location;
-				Size = Properties.Settings.Default.Size;
+				Location = Settings.Default.Location;
+				Size = Settings.Default.Size;
 			}
 		}
 
@@ -2558,26 +2558,26 @@ namespace SADXModManager
 		{
 			if (WindowState == FormWindowState.Maximized)
 			{
-				Properties.Settings.Default.Location = RestoreBounds.Location;
-				Properties.Settings.Default.Size = RestoreBounds.Size;
-				Properties.Settings.Default.Maximised = true;
-				Properties.Settings.Default.Minimised = false;
+				Settings.Default.Location = RestoreBounds.Location;
+				Settings.Default.Size = RestoreBounds.Size;
+				Settings.Default.Maximised = true;
+				Settings.Default.Minimised = false;
 			}
 			else if (WindowState == FormWindowState.Normal)
 			{
-				Properties.Settings.Default.Location = Location;
-				Properties.Settings.Default.Size = Size;
-				Properties.Settings.Default.Maximised = false;
-				Properties.Settings.Default.Minimised = false;
+				Settings.Default.Location = Location;
+				Settings.Default.Size = Size;
+				Settings.Default.Maximised = false;
+				Settings.Default.Minimised = false;
 			}
 			else
 			{
-				Properties.Settings.Default.Location = RestoreBounds.Location;
-				Properties.Settings.Default.Size = RestoreBounds.Size;
-				Properties.Settings.Default.Maximised = false;
-				Properties.Settings.Default.Minimised = true;
+				Settings.Default.Location = RestoreBounds.Location;
+				Settings.Default.Size = RestoreBounds.Size;
+				Settings.Default.Maximised = false;
+				Settings.Default.Minimised = true;
 			}
-			Properties.Settings.Default.Save();
+			Settings.Default.Save();
 		}
 
 		private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
