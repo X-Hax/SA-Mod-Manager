22fa51e7a3f7047f34ffa6451d2ba4407fcff11c
ItsEasyActually <darkspines35@gmail.com>
2023-06-28 23:07:17 -0400

    Created new ImageButton class and two styles for creating buttons with images. Added missing strings to English Landuage. Updated DarkTheme to include new colors. Updated most of MainWindow to make use of the new classes

--- ModManagerWPF/App.xaml.cs
+++ ModManagerWPF/App.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Navigation;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace ModManagerWPF
 {
@@ -233,5 +234,92 @@ namespace ModManagerWPF
 			window.Close();
 		}
     }
+
+	public partial class ImageButton : Button
+	{
+		public enum DrawType
+		{
+			Path = 0,
+			Fill = 1,
+		}
+
+		public Geometry Icon
+		{
+			get
+			{
+				return GetValue(IconProperty) as Geometry;
+			}
+			set
+			{
+				SetValue(IconProperty, value);
+			}
+		}
+
+		public static readonly DependencyProperty IconProperty =
+			DependencyProperty.Register("Icon", typeof(Geometry), typeof(ImageButton));
+
+		public Brush ImageBrush
+		{
+			get
+			{
+				return GetValue(ImageBrushProperty) as Brush;
+			}
+			set
+			{
+				SetValue(ImageBrushProperty, value);
+			}
+		}
+
+		public static readonly DependencyProperty ImageBrushProperty = 
+			DependencyProperty.Register("ImageBrush", typeof(Brush), typeof(ImageButton));
+
+		public Brush ImageBrushDisabled
+		{
+			get
+			{
+				return GetValue(ImageBrushDisabledProperty) as Brush;
+			}
+			set
+			{
+				SetValue(ImageBrushDisabledProperty, value);
+			}
+		}
+
+		public static readonly DependencyProperty ImageBrushDisabledProperty = 
+			DependencyProperty.Register("ImageBrushDisabled", typeof(Brush), typeof(ImageButton));
+
+		public Brush ImageBrushHover
+		{
+			get
+			{
+				return GetValue(ImageBrushHoverProperty) as Brush;
+			}
+			set
+			{
+				SetValue(ImageBrushHoverProperty, value);
+			}
+		}
+
+		public static readonly DependencyProperty ImageBrushHoverProperty = 
+			DependencyProperty.Register("ImageBrushHover", typeof(Brush), typeof(ImageButton));
+
+		public double IconThickness
+		{
+			get { return (double)GetValue(IconThicknessProperty); }
+			set { SetValue(IconThicknessProperty, value); }
+		}
+
+		public static readonly DependencyProperty IconThicknessProperty =
+			DependencyProperty.Register("IconThickness", typeof(double), typeof(ImageButton));
+
+		public bool IconFill
+		{
+			get { return (bool)GetValue(IconFillProperty); }
+			set { SetValue(IconFillProperty, value); }
+		}
+
+		public static readonly DependencyProperty IconFillProperty = 
+			DependencyProperty.Register("IconFill", typeof(bool), typeof(ImageButton));
+	}
 }
 
