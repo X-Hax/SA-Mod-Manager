821b5ff1ae1e459d2f4fa478525f6b12baafa02a
MainMemory <sonicmike2@yahoo.com>
2023-07-31 20:24:29 -0500

    Adding support for loading character welds from mod.ini.

--- SADXModLoader/dllmain.cpp
+++ SADXModLoader/dllmain.cpp
@@ -1039,6 +1039,48 @@ static vector<string> split(const string& s, char delim)
 	return elems;
 }
 
+static string trim(const string& s)
+{
+	auto st = s.find_first_not_of(' ');
+
+	if (st == string::npos)
+	{
+		st = 0;
+	}
+
+	auto ed = s.find_last_not_of(' ');
+
+	if (ed == string::npos)
+	{
+		ed = s.size() - 1;
+	}
+
+	return s.substr(st, (ed + 1) - st);
+}
+
+static const unordered_map<string, uint8_t> charnamemap = {
+	{ "sonic",    Characters_Sonic },
+	{ "eggman",   Characters_Eggman },
+	{ "tails",    Characters_Tails },
+	{ "knuckles", Characters_Knuckles },
+	{ "tikal",    Characters_Tikal },
+	{ "amy",      Characters_Amy },
+	{ "gamma",    Characters_Gamma },
+	{ "big",      Characters_Big }
+};
+
+static uint8_t ParseCharacter(const string& str)
+{
+	string str2 = trim(str);
+	transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+	auto ch = charnamemap.find(str2);
+	if (ch != charnamemap.end())
+		return ch->second;
+	else
+		return (uint8_t)strtol(str.c_str(), nullptr, 10);
+}
+extern void RegisterCharacterWelds(const uint8_t character, const char* iniPath);
+
 LoaderSettings loaderSettings = {};
 std::vector<Mod> modlist;
 static void __cdecl InitMods()
@@ -1433,7 +1475,7 @@ static void __cdecl InitMods()
 			modinfo->getBool("RedirectChaoSave"),
 			{
 				deparr,
-				moddeps.size()
+				(int)moddeps.size()
 			}
 		};
 
@@ -1468,6 +1510,14 @@ static void __cdecl InitMods()
 				fileswaps.emplace_back(FileMap::normalizePath(iter.first),
 					FileMap::normalizePath(iter.second));
 			}
+		}
+
+		if (ini_mod->hasGroup("CharacterWelds"))
+		{
+			const IniGroup* group = ini_mod->getGroup("CharacterWelds");
+			auto data = group->data();
+			for (const auto& iter : *data)
+				RegisterCharacterWelds(ParseCharacter(iter.first), iter.second.c_str());
 		}
 
 		// Check for SYSTEM replacements.
