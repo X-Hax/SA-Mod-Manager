b2d4c761554e7c3dc2f4095d3ff7b8bbfc360465
ItsEasyActually <darkspines35@gmail.com>
2023-11-20 23:34:51 -0500

    Added CustomWindowEnabledConverter for Custom Resolution variables.

--- SA-Mod-Manager/Elements/Converters.cs
+++ SA-Mod-Manager/Elements/Converters.cs
@@ -97,4 +97,25 @@ namespace SAModManager.Elements
 			return Binding.DoNothing;
 		}
 	}
+
+	public class CustomWindowConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value is int index)
+			{
+				if (index == 3)
+					return true;
+				else
+					return false;
+			}
+
+			return Binding.DoNothing;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return Binding.DoNothing;
+		}
+	}
 }
