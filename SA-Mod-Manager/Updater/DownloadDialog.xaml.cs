3024bec1618e17cef522c22d013d804e2bfe2908
Sora <sora.x@hotmail.fr>
2023-11-25 18:34:28 +0100

    Rework and fix offline installation for Loader and Dependencies

--- SA-Mod-Manager/Updater/DownloadDialog.xaml.cs
+++ SA-Mod-Manager/Updater/DownloadDialog.xaml.cs
@@ -22,9 +22,11 @@ namespace SAModManager.Updater
         private readonly string fileName;
         private string dest = ".SATemp";
         private readonly CancellationTokenSource tokenSource = new();
+        public Action<Exception> DownloadFailed;
         public bool done = false;
         private DLType Type = DLType.Download;
         public Action DownloadCompleted;
+        public bool failsafe = false;
 
 
         public enum DLType
@@ -34,16 +36,15 @@ namespace SAModManager.Updater
             Update
         }
 
-        public DownloadDialog(Uri uri, string title, string fileName, string dest = null, DLType type = DLType.Download)
+        public DownloadDialog(Uri uri, string title, string fileName, string dest = null, DLType type = DLType.Download, bool failsafe = false)
         {
             InitializeComponent();
-
 
             switch (type)
             {
                 case DLType.Download:
                 default:
-                    Title =  Lang.GetString("Updater.DL.Dep.Download") + " - ";
+                    Title = Lang.GetString("Updater.DL.Dep.Download") + " - ";
                     break;
                 case DLType.Install:
                     Title = Lang.GetString("Updater.DL.Dep.Install") + " - ";
@@ -58,7 +59,7 @@ namespace SAModManager.Updater
 
             this.Type = type;
 
-
+            this.failsafe = failsafe;
             Title += title;
             DLInfo.Text += " " + title + "...";
             this.fileName = fileName;
@@ -76,55 +77,6 @@ namespace SAModManager.Updater
             }
             catch { }
 
-        }
-
-        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                DLProgress.Value = e.ProgressPercentage;
-            });
-        }
-
-        private async void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
-        {
-            await Application.Current.Dispatcher.Invoke(async () =>
-            {
-                switch (Type)
-                {
-                    case DLType.Download:
-                    default:
-                        DLInfo.Text = Lang.GetString("Updater.DL.Dep.Download") + " ";
-                        break;
-                    case DLType.Install:
-                        DLInfo.Text = Lang.GetString("Updater.DL.Dep.Install") + " ";
-                        break;
-                    case DLType.Update:
-                        DLInfo.Text = Lang.GetString("Updater.DL.Dep.Update") + " ";
-                        break;
-                }
-
-                DLInfo.Text += Lang.GetString("Updater.DL.Dep.Completed") + "\n" + Lang.GetString("Updater.DL.Dep.Copying");
-
-                await Task.Delay(200);
-                if (File.Exists(fileName) && Directory.Exists(dest))
-                {
-                    FileInfo fileInfo = new(fileName);
-
-                    if (fileInfo is not null && fileInfo.Length > 0)
-                        await Util.MoveFile(fileName, Path.Combine(dest, fileName), true);
-                }
-
-                done = true;
-
-                await Task.Delay(200);
-
-                if (!string.IsNullOrEmpty(dest) && File.Exists(fileName) && dest != Path.GetDirectoryName(Environment.ProcessPath))
-                    File.Delete(fileName);
-
-
-                this.Close();
-            });
         }
 
         private async Task DownloadFileCompleted()
@@ -186,7 +138,7 @@ namespace SAModManager.Updater
                     }
                 }
                 catch { }
-   
+
                 await DownloadFileAsync(httpClient, uri.AbsoluteUri, destination).ConfigureAwait(false);
                 await Dispatcher.InvokeAsync(() =>
                 {
@@ -194,25 +146,27 @@ namespace SAModManager.Updater
                     DownloadCompleted?.Invoke();
                 });
             }
-            catch (AggregateException ae)
+            catch (Exception ex)
             {
-                // Handle the exception
-                ae.Handle(ex =>
-                {
-                    string s = Lang.GetString("MessageWindow.Errors.GenericDLFail0") + this.fileName + "\n" + ex.Message + "\n\n" + Lang.GetString("Lang.GetString(\"MessageWindow.Errors.GenericDLFail1");
 
-                    var error = new MessageWindow(Lang.GetString("MessageWindow.Errors.GenericDLFail.Title"), s, MessageWindow.WindowType.IconMessage, MessageWindow.Icons.Error, MessageWindow.Buttons.RetryCancel);
-                    error.ShowDialog();
-                    retry = error.isRetry;
-                    return true;
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    DownloadFailed?.Invoke(ex);
+                    Close();
                 });
+
+
+                /*string s = Lang.GetString("MessageWindow.Errors.GenericDLFail0") + this.fileName + "\n" + ex.Message + "\n\n" + Lang.GetString("Lang.GetString(\"MessageWindow.Errors.GenericDLFail1");
+                var error = new MessageWindow(Lang.GetString("MessageWindow.Errors.GenericDLFail.Title"), s, MessageWindow.WindowType.IconMessage, MessageWindow.Icons.Error, MessageWindow.Buttons.RetryCancel);
+                error.ShowDialog();
+                retry = error.isRetry;*/
             }
 
         }
 
-        public async Task DownloadFileAsync(HttpClient httpClient, string url, string destinationFilePath)
+        public async Task DownloadFileAsync(HttpClient httpClient, string url, string destinationFilePath, CancellationToken cancellationToken = default)
         {
-            using HttpResponseMessage response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+            using HttpResponseMessage response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
             response.EnsureSuccessStatusCode();
 
             using Stream contentStream = await response.Content.ReadAsStreamAsync();
@@ -235,6 +189,7 @@ namespace SAModManager.Updater
                     Dispatcher.Invoke(() => DLProgress.Value = percentage);
                 }
             }
+
         }
 
         public void StartDL()
---
+++ SA-Mod-Manager/Updater/OfflineInstall.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SAModManager.Updater
+{
+    /// <summary>
+    /// Interaction logic for OfflineInstall.xaml
+    /// </summary>
+    public partial class OfflineInstall : Window
+    {
+        public OfflineInstall(string file)
+        {
+            InitializeComponent();
+            FailedText.Text = string.Format(Lang.GetString("Updater.DL.Offline.Failed"), file);
+        }
+
+        public void CheckSuccess(bool success)
+        {
+            InstallProgress.Value = 100;
+
+            if (success)
+            {
+                StatusText.Text = Lang.GetString("CommonStrings.Success");
+            }
+            else
+            {
+                StatusText.Text = Lang.GetString("CommonStrings.Failed");
+            }
+        }
+    }
+}
