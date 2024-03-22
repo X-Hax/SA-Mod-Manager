549d381ae8906ab7abbb021700b0e0d71e12dc83
Sora <sora.x@hotmail.fr>
2024-03-22 15:44:52 +0100

    fix crash if no game are detected

--- SA-Mod-Manager/UI/MainWindow.xaml.cs
+++ SA-Mod-Manager/UI/MainWindow.xaml.cs
@@ -1701,7 +1701,7 @@ namespace SAModManager
 
                 if (App.GamesList is not null && (App.CurrentGame.loader is null || Directory.Exists(App.CurrentGame.gameDirectory) == false))
                 {
-                    if (App.Current.MainWindow is not null)
+                    if (App.Current.MainWindow is not null && App.GamesList.Count > 0)
                         ((MainWindow)App.Current.MainWindow).ComboGameSelection_SetNewItem(App.GamesList[0]);
                 }
             }
