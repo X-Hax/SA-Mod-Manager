05d594dc616b73724462f9605bcdcfa29521cb0c
Sora <sora.x@hotmail.fr>
2024-06-20 14:52:05 +0200

    Avalonia? Avalonia!

---
+++ SAModManager_Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SAModManager_Avalonia.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ SAModManager_Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SAModManager_Avalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
