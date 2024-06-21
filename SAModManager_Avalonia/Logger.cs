3a81007dc72ae968ebf3bec43cda7aa4fac83a3b
Sora <sora.x@hotmail.fr>
2024-06-21 20:21:13 +0200

    Implement logger, startup and util

---
+++ SAModManager_Avalonia/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace SAModManager_Avalonia
+{
+    public static class Logger
+    {
+        private static readonly string logFilePath = "application.log";
+
+        public static void Log(string message)
+        {
+            if (App.isDebug == false)
+                return;
+
+            try
+            {
+                string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}";
+                File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing to log file: {ex.Message}");
+            }
+        }
+    }
+
+
+}
