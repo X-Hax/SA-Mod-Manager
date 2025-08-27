6cfb3e96d77d0b53c4383df744eac8d4ef09c3d5
ItsEasyActually <darkspines35@gmail.com>
2025-08-27 15:23:09 -0400

    Adding new ModEntry class.

---
+++ SAMM.Configuration/Mods/ModEntry.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAMM.Configuration.Mods
+{
+	/// <summary>
+	/// Stores info on Mods that are found by the Manager
+	/// </summary>
+	public class ModEntry
+	{
+		#region Variables
+		public ModInfo? ModInfo { get; set; }
+
+		public string Directory { get; private set; } = string.Empty;
+
+		public bool IsEnabled { get; set; } = false;
+
+		public bool HasConfig { get; set; } = false;
+
+		#endregion
+
+		#region Constructors
+		public ModEntry() { }
+
+		public ModEntry(string directory)
+		{
+			if (Path.Exists(directory))
+			{
+				if (File.Exists(Path.Combine(directory, "mod.ini")))
+				{
+					Directory = directory;
+
+				}
+			}
+		}
+
+		#endregion
+
+		#region Functions
+
+
+		#endregion
+	}
+}
