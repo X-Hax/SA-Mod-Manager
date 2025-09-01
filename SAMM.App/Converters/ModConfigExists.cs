78fe33bf7ebdc20b6477382ae1c8271fe1348aeb
ItsEasyActually <darkspines35@gmail.com>
2025-09-01 16:19:09 -0400

    Adding converters for ModConfig and GameDirectory validation.

---
+++ SAMM.App/Converters/ModConfigExists.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Data.Converters;
+using HtmlAgilityPack;
+using SAMM.Configuration.Mods;
+using System;
+using System.Globalization;
+
+namespace SAMM.App.Converters
+{
+	/// <summary>
+	/// Handles conversion of a SelectedItem to a ModEntry and returns the Config value.
+	/// </summary>
+	public class ModConfigExists : IValueConverter
+	{
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			ModEntry entry = value as ModEntry;
+			if (entry != null) 
+				return entry.HasConfig;
+			else
+				return false;
+		}
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ SAMM.App/Converters/ValidGameConverter.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Data.Converters;
+using SAMM.Configuration;
+using SAMM.Configuration.Mods;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAMM.App.Converters
+{
+	/// <summary>
+	/// Validates if the Game Config that is currently set is valid or not.
+	/// </summary>
+	public class ValidGameConverter : IValueConverter
+	{
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			GameConfig config = (GameConfig)value;
+			if (config != null)
+			{
+				return Path.Exists(config.GameDirectory);
+			}
+			else
+				return false;
+		}
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
