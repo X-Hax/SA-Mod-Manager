0d195003609e99695d21f071c325fc590507dd16
ItsEasyActually <darkspines35@gmail.com>
2025-09-01 16:22:01 -0400

    Updates to existing temp work on the MainWindow and MainWindowViewModel.

--- SAMM.App/Models/MainWindowViewModel.cs
+++ SAMM.App/Models/MainWindowViewModel.cs
@@ -21,12 +21,20 @@ namespace SAMM.App.Models
 		#region Constructors
 		public MainWindowViewModel()
 		{
+			// The following code is essentially a unit test for the visual component of the SAMM for development.
+			// This will all be removed once the UI is mostly complete for the MainWindow and replaced with a proper initializer system.
+			GameConfig = SAMM.Configuration.GameConfig.GetGameConfig(Configuration.Enumeration.GameIDs.SADX, "test");
 			ModEntry mod = new ModEntry();
 			mod.ModInfo = new ModInfo();
 			mod.ModInfo.Name = "Test";
 			mod.ModInfo.Description = "This is a test description.";
 			mod.ModInfo.Author = "ItsATestActually";
 			mod.ModInfo.Version = "1.0.0";
+			mod.HasConfig = true;
+			ModEntries.Add(mod);
+			ModEntries.Add(mod);
+			ModEntries.Add(mod);
+			ModEntries.Add(mod);
 			ModEntries.Add(mod);
 		}
 
